2950f6af74b8aa47f4f0383f9592cae21d3053cd
Fernanda Alcantara <fernandamoedano20@gmail.com>
2025-03-19 16:23:54 -0600

    Proyecto versión Final

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+//----------------------
+using apiRESTCheckUsuario.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+
+namespace apiRESTCheckUsuario.Controllers
+{
+    public class UsuarioController : ApiController {
+
+        [HttpPost]
+        [Route("check/usuario/spinsusuario")]
+        public clsApiStatus spInsUsuario([FromBody] clsUsuario modelo) {
+            //Definicion de los objetos de modelos
+            clsApiStatus objRespuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+            //---------------
+            DataSet ds = new DataSet();
+            try
+            {
+                clsUsuario objUsuario = new clsUsuario(modelo.nombre,
+                                                       modelo.apellidoPaterno,
+                                                       modelo.apellidoMaterno,
+                                                       modelo.usuario,
+                                                       modelo.contrasena,
+                                                       modelo.ruta,
+                                                       modelo.tipo);
+                ds = objUsuario.spInsUsuario();
+
+                //configuracion del objeto de salida
+                objRespuesta.statusExec = true;
+                objRespuesta.ban = int.Parse
+                                        (ds.Tables[0].Rows[0][0].ToString());
+
+                //Validar valor de ban
+                if (objRespuesta.ban == 0){
+
+                    objRespuesta.msg = "Usuario registrado exitosamente";
+                    jsonResp.Add("msgData", "Usuario registrado exitosamente");
+                }
+
+                else {
+                    objRespuesta.msg = "Usuario no registrado, verificar...";
+                    jsonResp.Add("msgData", "Usuario no registrado, verificar...");
+
+                }
+
+                objRespuesta.datos = jsonResp;
+            }
+
+            catch(Exception ex){
+                //configuracion del objeto de salida
+                objRespuesta.statusExec = false;
+                objRespuesta.ban = -1;
+                objRespuesta.msg = "Falló la inserción de usuario, verificar...";
+                jsonResp.Add("msgData", ex.Message.ToString());
+                objRespuesta.datos = jsonResp;
+            }
+
+            //salida del objeto ObjRespuesta
+            return objRespuesta;
+        }
+
+        // endpoint para validación de acceso spValidarAcceso
+        [HttpPost]
+        [Route("check/usuario/spvalidaracceso")]
+        public clsApiStatus spValidarAcceso([FromBody] clsUsuario modelo)
+        {
+            // -----------------------------------------
+            clsApiStatus objRespuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+            // -----------------------------------------
+            DataSet ds = new DataSet();
+            try
+            {
+                // Creación del objeto del modelo clsUsuario
+                clsUsuario objUsuario = new clsUsuario(modelo.usuario,
+                                                       modelo.contrasena);
+                ds = objUsuario.spvalidarAcceso();
+
+                // Configuración del objeto de salida
+
+                objRespuesta.statusExec = true;
+                objRespuesta.ban = int.Parse(ds.Tables[0].Rows[0][0].ToString());
+
+                //validar el valor recibido en bandera
+                if (objRespuesta.ban == 1)
+                {
+                    objRespuesta.msg = "Usuario validado exitosamente";
+                    jsonResp.Add("usu_nombre_completo", ds.Tables[0].Rows[0][1].ToString());
+                    jsonResp.Add("usu_ruta", ds.Tables[0].Rows[0][2].ToString());
+                    jsonResp.Add("usu_usuario", ds.Tables[0].Rows[0][3].ToString());
+                    jsonResp.Add("tip_descripcion", ds.Tables[0].Rows[0][4].ToString());
+                    objRespuesta.datos = jsonResp;
+                }
+                else
+                {
+                    objRespuesta.msg = "Acceso denegado, verificar...";
+                    jsonResp.Add("msgData", "Acceso denegado, verificar...");
+                    objRespuesta.datos = jsonResp;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Configuración del objeto de salida
+
+                objRespuesta.statusExec = false;
+                objRespuesta.ban = -1;
+                objRespuesta.msg = "Error de conexión con el servicio de datos";
+                jsonResp.Add("msgData", ex.Message.ToString());
+                objRespuesta.datos = jsonResp;
+
+            }
+
+            //retorno del obj de salida objespuesta
+            return objRespuesta;
+
+        }//fin del endpoint
+
+
+        // endpoint para validación para usuarios vwrtpusuario
+        [HttpGet]
+        [Route("check/usuario/vwRptUsuario")]
+        public clsApiStatus vwRptUsuario(string filtro = "")
+        {
+            // -----------------------------------------
+            clsApiStatus objRespuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+            // -----------------------------------------
+            DataSet ds = new DataSet();
+
+            try
+            {
+                clsUsuario objUsuario = new clsUsuario();
+                ds = objUsuario.vwRptUsuario();
+
+                // Aplicar filtro si se proporciona
+                DataTable dt = ds.Tables[0];
+                if (!string.IsNullOrEmpty(filtro))
+                {
+                    DataView dv = new DataView(dt);
+                    dv.RowFilter = $"nombre LIKE '%{filtro}%'"; // Filtra por coincidencias parciales en 'nombre'
+                    dt = dv.ToTable(); // Convierte el DataView filtrado en un DataTable
+                }
+
+                //configuracion del objeto de salida 
+                objRespuesta.statusExec = true;
+                objRespuesta.ban = dt.Rows.Count;
+                objRespuesta.msg = "Reporte consultado exitosamente";
+
+                // Convertir el DataTable filtrado a JSON
+                string jsonString = JsonConvert.SerializeObject(dt, Formatting.Indented);
+                jsonResp = JObject.Parse($"{{\"{dt.TableName}\": {jsonString}}}");
+
+                objRespuesta.datos = jsonResp;
+            }
+            catch (Exception ex)
+            {
+                // Configuración del objeto de salida
+                objRespuesta.statusExec = false;
+                objRespuesta.ban = -1;
+                objRespuesta.msg = "Error de conexión con el servicio de datos";
+                jsonResp.Add("msgData", ex.Message.ToString());
+                objRespuesta.datos = jsonResp;
+            }
+
+            //retorno del objeto de salida
+            return objRespuesta;
+        }
+
+
+        // endpoint para validación para usuarios vwrtpusuario
+        [HttpGet]
+            [Route("check/usuario/vwTipoUsuario")]
+            public clsApiStatus vwTipoUsuario()
+            {
+                // -----------------------------------------
+                clsApiStatus objRespuesta = new clsApiStatus();
+                JObject jsonResp = new JObject();
+                // -----------------------------------------
+                DataSet ds = new DataSet();
+
+                try
+                {
+                    clsUsuario objUsuario = new clsUsuario();
+                    ds = objUsuario.vwTipoUsuario();
+                    //configuracion del objeto de salida 
+                    objRespuesta.statusExec = true;
+                    objRespuesta.ban = ds.Tables[0].Rows.Count;
+                    objRespuesta.msg = "Reporte consultado exitosamente";
+                    //formatear los datos recibidos del dataset para
+                    //enviarlos de salida json
+                    // Migración del ds(DataSet) al objeto Json
+                    string jsonString = JsonConvert.SerializeObject(ds.Tables[0], Formatting.Indented);
+                    jsonResp = JObject.Parse($"{{\"{ds.Tables[0].TableName}\": {jsonString}}}");
+
+
+                    objRespuesta.datos = jsonResp;
+                }
+                catch (Exception ex)
+                {
+                    // Configuración del objeto de salida
+
+                    objRespuesta.statusExec = false;
+                    objRespuesta.ban = -1;
+                    objRespuesta.msg = "Error de conexión con el servicio de datos";
+                    jsonResp.Add("msgData", ex.Message.ToString());
+                    objRespuesta.datos = jsonResp;
+                }
+                //retorno del objeto de salida
+                return objRespuesta;
+
+            }
+
+
+
+    }
+}
---
+++ Models/clsUsuario.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;//conectarnos a web config
+using System.Data;//recibir y enviar objetos contenedores de datos
+using MySql.Data.MySqlClient;
+
+namespace apiRESTCheckUsuario.Models
+{
+    public class clsUsuario {
+
+        // Definición de atributos
+        public string cve { get; set; }
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string usuario { get; set; }
+        public string contrasena { get; set; }
+        public string ruta { get; set; }
+        public string tipo { get; set; }
+
+        //Métodos y atributos de funcionalidad y seguridad
+        string cadConn = ConfigurationManager.ConnectionStrings["bd_control_acceso"].ConnectionString;
+
+        //Constructores de la clase que se usan en los endpoints
+        //primer constructor para las vistas
+        public clsUsuario(){
+
+        }
+
+
+        //segundo constructor para acceso
+        public clsUsuario(string usuario,string contrasena)
+        {
+            this.usuario = usuario;
+            this.contrasena = contrasena;
+        }
+
+        
+
+        //tercer constructor para todos los datos
+        public clsUsuario(string nombre,
+                          string apellidoPaterno,
+                          string apellidoMaterno,
+                          string usuario,
+                          string contrasena,
+                          string ruta,
+                          string tipo)
+        {
+            this.nombre = nombre;
+            this.apellidoPaterno = apellidoPaterno;
+            this.apellidoPaterno = apellidoMaterno;
+            this.usuario = usuario;
+            this.contrasena = contrasena;
+            this.ruta = ruta;
+            this.tipo = tipo;
+        }
+
+        //Metodo para la ejecución del spInUsuario
+        public DataSet spInsUsuario() {
+
+            string cadSql = "CALL spInsUsuario('" + this.nombre +
+                                                "', '" + this.apellidoPaterno +
+                                                "', '" + this.apellidoMaterno + 
+                                                "', '" + this.usuario +
+                                                "', '" + this.contrasena + 
+                                                "', '" + this.ruta + 
+                                                "',  " + this.tipo + ")";
+
+
+            // Configuración de los objetos de conexión a datos
+            MySqlConnection cnn = new MySqlConnection(cadConn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cadSql, cnn);
+            DataSet ds = new DataSet();
+
+            //Ejecucion del adaptador de datos (retorna un DataSet)
+            da.Fill(ds, "spInsUsuario");
+
+            //retorno de los datos recibidos
+            return ds;
+        }
+        // Proceso de validación de usuarios (spValidarAcceso)
+        public DataSet spvalidarAcceso()
+        {
+            // Crear el comando SQL
+            string cadSQL = "";
+            cadSQL = "call spvalidarAcceso('" + this.usuario + "','"
+                                              + this.contrasena + "');";
+            // Configuración de objetos de conexión
+            MySqlConnection cnn = new MySqlConnection(cadConn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cadSQL, cnn);
+            DataSet ds = new DataSet();
+            // Ejecución y salida
+            da.Fill(ds, "spvalidarAcceso");
+            return ds;
+        }
+
+
+        // Proceso de consulta de usuarios (viewrtpusuario)
+        public DataSet vwRptUsuario()
+        {
+            // Crear el comando SQL
+            string cadSQL = "";
+            cadSQL = "SELECT * FROM vwRptUsuario";
+            // Configuración de objetos de conexión
+            MySqlConnection cnn = new MySqlConnection(cadConn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cadSQL, cnn);
+            DataSet ds = new DataSet();
+            // Ejecución y salida
+            da.Fill(ds, "vwRptUsuario");
+            return ds;
+        }
+
+        // Proceso de consulta de Tipo de Usuarios (vwtipousuario)
+        public DataSet vwTipoUsuario()
+        {
+            // Crear el comando SQL
+            string cadSQL = "";
+            cadSQL = "SELECT * FROM vwTipoUsuario";
+            // Configuración de objetos de conexión
+            MySqlConnection cnn = new MySqlConnection(cadConn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cadSQL, cnn);
+            DataSet ds = new DataSet();
+            // Ejecución y salida
+            da.Fill(ds, "vwTipoUsuario");
+            return ds;
+        }
+
+
+    }
+}
